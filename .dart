ae9b12af9d80326ccacb9c14705a0d13a8d9610a
MarvinRem <67871529+MarvinRem@users.noreply.github.com>
2020-07-07 12:40:35 +0200

    Create .dart

---
+++ .dart
@@ -0,0 +1,11 @@
+main(){
+  
+  String meinName = 'Marvin';
+  print('mein Name ist: $meinName ');
+  
+  double zahl = 5.2;
+  String stringzahl = zahl.toString();
+  print(stringzahl);
+  
+  print(stringzahl.length);
+  }
